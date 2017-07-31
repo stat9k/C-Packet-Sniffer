28fcb972fa2d877825d2cb0342737e187dd4ed36
jackmac <status.obrien@gmail.com>
2017-07-31 21:28:44 +1200

    Instantiated ipv4 protocol. Created ipv4 struct using types to define byte structure of ipv4 header

--- main.c
+++ main.c
@@ -17,6 +17,7 @@
 #include <stdio.h>
 #include <pcap.h>
 #include "packet_headers.h"
+#include <arpa/inet.h>
 
 
 /**
@@ -26,18 +27,45 @@
  * @param packet - the parsed data from tcpdump/pcap_file
  * @return - the ethernet protocol - IPv4, IPv6, etc...
  */
-short unpack_ethernet_header_packet (const u_char *packet)
-{
+int unpack_ethernet_header_frame (const u_char *packet);
 
-    struct ethernet_header *eth_packet = (struct ethernet_header*) packet;
+
+/**
+ * Unpacks the IPv4 Packet, prints out the ipv4 header info
+ *
+ * @param packet - the parsed data from tcpdump + ETHERNET_HEADER_LENGTH
+ */
+void unpack_ipv4_packet (const u_char *packet);
+
+/**
+ * Prints the IPv4 address in the form of 127.0.0.1
+ *
+ * @param address - the bytes to be converted
+ */
+void get_ipv4_address (char *string, __uint32_t address);
+
+
+/**
+ * The main function for parsing a packet
+ *
+ * @param args
+ * @param header
+ * @param packet
+ */
+void got_packet (u_char *args, const struct pcap_pkthdr *header, const u_char *packet);
+
+
+int unpack_ethernet_header_frame(const u_char *packet) {
+
+    struct ethernet_frame *eth_frame = (struct ethernet_frame *) packet;
 
     printf("Source Mac");
     for (int i=0; i<ETH_ADDRESS_LENGTH; i++)
     {
         if (i == 0)
-            printf(": %02x", eth_packet->source_mac_addr[i]);
+            printf(": %02x", eth_frame->source_mac_addr[i]);
         else
-            printf(":%02x", eth_packet->source_mac_addr[i]);
+            printf(":%02x", eth_frame->source_mac_addr[i]);
     }
     printf("\n");
 
@@ -45,23 +73,57 @@ short unpack_ethernet_header_packet (const u_char *packet)
     for (int i=0; i<ETH_ADDRESS_LENGTH; i++)
     {
         if (i == 0)
-            printf(": %02x", eth_packet->dest_mac_addr[i]);
+            printf(": %02x", eth_frame->dest_mac_addr[i]);
         else
-            printf(":%02x", eth_packet->dest_mac_addr[i]);
+            printf(":%02x", eth_frame->dest_mac_addr[i]);
     }
     printf("\n");
+
+    return eth_frame->protocol;
 }
 
-void got_packet (u_char *args, const struct pcap_pkthdr *header, const u_char *packet)
-{
+void unpack_ipv4_packet(const u_char *packet) {
+
+    struct ipv4_packet *ip_packet = (struct ipv4_packet *) packet;
+
+    printf("Version: %d\n", ip_packet->version);
+
+    get_ipv4_address("Source Address", ip_packet->src_addr);
+    get_ipv4_address("Destination Address", ip_packet->dest_addr);
+}
+
+void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet) {
     printf("Header Length: %d\n", header->len);
 
     // now we have the packet, we need to break it open
-    // we start with the ethernet_header
-    unpack_ethernet_header_packet(packet);
+    // we start with the ethernet_frame
+    int eth_proto = unpack_ethernet_header_frame(packet);
 
+    switch (eth_proto) {
 
+        case 8:                             // IPv4
+            printf("Protocol: IPv4\n");
+
+            // unpack the IPv4 packet
+            unpack_ipv4_packet(packet + ETH_HEADER_LENGTH);
+            break;
+
+        case 56710:                         // IPv6
+            printf("Protocol: IPv6\n");
+            break;
+
+        default:
+            break;
+    }
 }
+
+void get_ipv4_address(char *string, __uint32_t address) {
+    struct in_addr ip;
+    ip.s_addr = address;
+
+    printf("%s: %s\n", string, inet_ntoa(ip));
+}
+
 
 int main(int argc, char **argv)
 {
--- packet_headers.h
+++ packet_headers.h
@@ -11,8 +11,24 @@
 #define ETH_HEADER_LENGTH       14
 
 
-struct ethernet_header {
+struct ethernet_frame {
     unsigned char source_mac_addr[ETH_ADDRESS_LENGTH];
     unsigned char dest_mac_addr[ETH_ADDRESS_LENGTH];
     unsigned short protocol;
+};
+
+struct ipv4_packet {
+    unsigned char ihl : 4;
+    unsigned char version : 4;
+    unsigned char type_of_service;
+    unsigned short total_length;
+    unsigned short identification;
+    unsigned short flags_and_fragment_offset;
+    unsigned char time_to_live;
+    unsigned char protocol;
+    unsigned short header_checksum;
+    unsigned int src_addr;
+    unsigned int dest_addr;
+
+    // payload should be exposed by now...
 };
